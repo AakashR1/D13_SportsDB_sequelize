5a9d0c732ef45861e47320618cf67bb02461e1ad
Aakash Rathod <aakash.rathod@sarvadhi.com>
2022-06-06 14:46:37 +0530

    added some row query

--- src/controller/teamController.js
+++ src/controller/teamController.js
@@ -1,7 +1,7 @@
 const db= require('../DB/db');
 const Team = db.teams;
 const Player = db.player;
-
+const sequelize = db.sequelize;
 const addTeam =async (req, res) => {
     try {
        await Team.create(req.body);
@@ -68,11 +68,54 @@ const teamWithPlayer = async (req,res)=>{
         res.send(error);
     }
 }
+
+const rowQuery1 =async (req,res)=>{
+    try {
+        const [results, metadata]  = await sequelize.query('SELECT Players.player_name, Players.player_email , Teams.team_name  FROM Teams JOIN Players ON Players.team_id = Teams.team_id');
+        console.log(results);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const rowQuery2 =async (req,res)=>{
+    try {
+        const [results, metadata]  = await sequelize.query('SELECT team_name AS "A.b.c" FROM Teams WHERE team_id IN(:status)',
+        {
+          replacements: { status: [1,3] },
+          nest:true
+        }
+      );
+        console.log(results);
+    } catch (error) {
+        console.log(error);
+    }
+}
+rowQuery2();
+const rowQuery3 =async (req,res)=>{
+    try {
+        const [results, metadata]  = await sequelize.query('SELECT team_name FROM Teams WHERE team_id  = $',
+        {
+          replacements: { status: [1,3] },
+          nest:true
+        }
+      );
+        console.log(results);
+    } catch (error) {
+        console.log(error);        
+    }
+}
+
+
+
 module.exports = {
     addTeam,
     updateTeam,
     getAllTeamsAndId,
     getTeamName,
     deleteTeam,
-    teamWithPlayer
+    teamWithPlayer,
+    rowQuery1,
+    rowQuery2,
+    rowQuery3
 }
--- src/routes/teamRoutes.js
+++ src/routes/teamRoutes.js
@@ -11,8 +11,12 @@ routes.get('/getTeam/:Id',teamController.getTeamName);
 
 routes.get('/allTeams',teamController.getAllTeamsAndId);
 
-routes.delete('/deleteTeam/:Id',teamController.deleteTeam)
+routes.delete('/deleteTeam/:Id',teamController.deleteTeam);
 
-routes.get('/teamWithPlayer',teamController.teamWithPlayer)
+routes.get('/teamWithPlayer',teamController.rowQuery1);
+
+routes.get('/rowQuery2',teamController.rowQuery2);
+
+routes.get('/rowQuery3',teamController.rowQuery3);
 
 module.exports = routes;
